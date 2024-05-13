fcc21b973f8eb0ed783675b2083dadb88dfc70cf
danil <danil2savin@yandex.ru>
2024-05-13 23:45:42 +0300

    add simple replacement

--- src/screen/chat/MessageScreen.js
+++ src/screen/chat/MessageScreen.js
@@ -10,35 +10,7 @@ const MessageScreen = ({ route, navigation }) => {
     const [messages, setMessages] = useState([]);
     const [message, setMessage] = useState("");
 
-    useEffect(() => {
-        const getUserID = async() =>{
-            const response = await getUserId();
-            const temp = JSON.parse(response)
-            return  JSON.parse(temp).user_id // разобраться почему так работает
-        }
-        getUserID()
-            .then(r => setUserID(r))
-
-
-        const socketConnection = io(`${DB_HOST}`);
-        setSocket(socketConnection)
-        socketConnection.on("connect", () => {
-            console.log("socket connect status: ", socketConnection.connected)
-            socketConnection.emit("join", { first_name: "Данил" }, () => {
-                socketConnection.emit("joinRoom", { id: 1, name: "1992" })
-            })
-                socketConnection.emit("takeMessages", { room_id: 1, row: 0 })
-                socketConnection.on("getMessages", async (response) => {
-                    console.log(response)
-                    setMessages(response)
-            })
-            socketConnection.on("message", async (message) => {
-                console.log(message)
-                setMessages((prevMessages) => [...prevMessages, message]);
-            })
-        })
-        navigation.setOptions({ title: chat.name });
-    }, []);
+   п
     const messageObjectCreator= (message) => {
         const messageObject = {}
         messageObject.user_id = userID
